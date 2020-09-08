344c02af7891607b746d7f3dfa68d7d1017cef4b
stepJo <ephenjo99@gmail.com>
2020-09-08 12:20:24 +0700

    Create owner table

---
+++ database/migrations/2020_09_08_051757_create_owners_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOwnersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('owners', function (Blueprint $table) {
+            $table->id('own_id');
+            $table->string('own_name');
+            $table->string('own_email');
+            $table->string('own_contact');
+            $table->string('own_password');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('owners');
+    }
+}
