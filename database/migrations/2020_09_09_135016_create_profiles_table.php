66eada3397e495bd6252a6a6c4125884a73d2268
stepJo <ephenjo99@gmail.com>
2020-09-10 13:08:21 +0700

    Add profiles table migration and seeder

---
+++ database/migrations/2020_09_09_135016_create_profiles_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+            $table->string('app_name');
+            $table->string('app_email');
+            $table->string('app_contact')->nullable();
+            $table->string('app_address')->nullable();
+            $table->string('app_logo')->nullable();
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+}
