1ed60d8eba269c72b3e8bbee95cd87a36e34d442
stepJo <ephenjo99@gmail.com>
2020-09-14 16:12:34 +0700

    Add customer model

--- app/Models/MCustomer/Customer.php
+++ app/Models/MCustomer/Customer.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Customer extends Model
 {
-    //
+    protected $primaryKey = 'c_id';
+
+    protected $fillable = [
+        'c_id',
+        'c_name',
+        'c_email',
+        'c_contact',
+        'c_address',
+        'created_at',
+        'updated_at'
+    ];
 }
